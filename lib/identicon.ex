252dc6129747709683507d31d507d960079c09d9
Stephen Gladney <stephengladney@gmail.com>
2023-12-11 23:26:54 -0500

    add pixel map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -41,7 +41,21 @@ defmodule Identicon do
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     grid_filtered = Enum.filter(grid, fn {code, _index} -> rem(code, 2) == 0 end)
-
     %Identicon.Image{image | grid: grid_filtered}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 end
