348bf92b3bd055c4500e03d4d075aeba9c8564e4
Stephen Gladney <stephengladney@gmail.com>
2023-12-11 22:57:54 -0500

    hash, get color, build grid and filter odds

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,10 +6,42 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input()
+    |> pick_color()
+    |> build_grid()
+    |> filter_odd_squares()
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list()
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    tail = Enum.reverse(row) |> Enum.slice(1..-1)
+    Enum.concat(row, tail)
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid_filtered = Enum.filter(grid, fn {code, _index} -> rem(code, 2) == 0 end)
+
+    %Identicon.Image{image | grid: grid_filtered}
   end
 end
